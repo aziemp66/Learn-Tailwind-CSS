54ac0ad7df1a96cee3ef740818fc4a781d407d18
Azie Melza Pratama <aziemp66@gmail.com>
2022-05-15 10:26:45 +0700

    Leaning Width and Height

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ function App() {
 		<div>
 			<h1 className="text-4xl text-center">Belajar Tailwind</h1>
 
-			<section className="mx-auto my-6">
+			<section className="mb-10">
 				<h2>Spacing</h2>
 				<div className="ml-8 ">Margin</div>
 				<div className="py-9">Padding</div>
+			</section>
+
+			<section className="mb-10">
+				<h2>Sizing</h2>
+				<div className="w-32 h-32 mx-auto text-center ">Kotak 1</div>
+				<div className="max-w-xs min-h-screen">Kotak 2</div>
 			</section>
 		</div>
 	);
