457430e86652bfd1bf567b29fc77476dfa0c6556
Azie Melza Pratama <aziemp66@gmail.com>
2022-05-15 10:08:32 +0700

    Marginization with Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-	return <div className="App">Hemlo</div>;
+	return (
+		<div>
+			<h1 className="text-4xl text-center">Belajar Tailwind</h1>
+
+			<section className="my-8">
+				<h2>Spacing</h2>
+				<div className="ml-8 ">Margin</div>
+			</section>
+		</div>
+	);
 }
 
 export default App;
