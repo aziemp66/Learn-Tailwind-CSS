5d223d9171b6a5da8058f964b166a97682c76d7c
Azie Melza Pratama <aziemp66@gmail.com>
2022-05-15 20:36:56 +0700

    Learn Background Clipping, Size, Repeat, Position

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import bg from "./Assets/1.jpg";
+
 function App() {
 	return (
 		<div>
@@ -66,9 +68,81 @@ function App() {
 			</section>
 
 			<div
-				className="max-w-xl h-96 bg-slate-500 mx-auto mt-[42px] mb-14"
+				className="max-w-xl h-96 bg-slate-500 mx-auto mt-[42px] mb-14 bg-fixed overflow-y-scroll"
 				style={{
 					backgroundImage: `url(https://source.unsplash.com/1000x600?frontend)`,
+				}}
+			>
+				<p className="p-5 text-gray-100 text-2xl">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Quos perspiciatis dolores dolor dignissimos. Maiores rem,
+					at, placeat accusamus reprehenderit, autem nobis ipsum animi
+					adipisci officia dolorum est! Non possimus dolorem ab at,
+					officiis hic error deleniti voluptatum reprehenderit esse
+					provident, aliquam corporis quis laudantium sunt velit quae.
+					Autem fuga perferendis ea. Inventore non porro obcaecati at
+					et unde illo ipsum fugiat earum minima voluptatibus enim
+					dolores tempora eveniet modi pariatur eum eos, ipsa magni ex
+					necessitatibus quos placeat reiciendis! Corrupti quos
+					provident modi amet laborum iusto dolorum quis impedit,
+					ipsum reprehenderit possimus ut, at aspernatur expedita
+					ullam, fugiat laudantium repellendus cumque. Tempore
+					aspernatur et porro delectus vel doloribus nemo. Debitis,
+					similique ipsam. Culpa fugiat, corrupti laborum quis
+					voluptatem quidem quibusdam veniam voluptate ipsum ea non
+					excepturi impedit, doloremque beatae praesentium esse iure
+					accusantium nam natus. Impedit soluta reprehenderit ex odit
+					cupiditate recusandae delectus ullam nihil. Facilis,
+					eligendi ullam quam tempora suscipit qui alias debitis, at
+					reiciendis dolores corporis recusandae sit beatae
+					praesentium? Quia odio harum voluptates. Dolore hic vel
+					nulla nesciunt aut iste, rerum, eos dolorem tenetur quos
+					perferendis adipisci labore unde eius at et? Ad, molestias.
+					Deserunt accusamus, rem, natus, eius molestiae nemo fugit
+					doloremque eos ut minus doloribus.
+				</p>
+			</div>
+
+			<div
+				className="max-w-xl h-96 bg-slate-500 mx-auto mt-[42px] mb-14 bg-scroll overflow-y-scroll"
+				style={{
+					backgroundImage: `url(https://source.unsplash.com/1000x600?frontend)`,
+				}}
+			>
+				<p className="p-5 text-gray-50 text-2xl">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Quos perspiciatis dolores dolor dignissimos. Maiores rem,
+					at, placeat accusamus reprehenderit, autem nobis ipsum animi
+					adipisci officia dolorum est! Non possimus dolorem ab at,
+					officiis hic error deleniti voluptatum reprehenderit esse
+					provident, aliquam corporis quis laudantium sunt velit quae.
+					Autem fuga perferendis ea. Inventore non porro obcaecati at
+					et unde illo ipsum fugiat earum minima voluptatibus enim
+					dolores tempora eveniet modi pariatur eum eos, ipsa magni ex
+					necessitatibus quos placeat reiciendis! Corrupti quos
+					provident modi amet laborum iusto dolorum quis impedit,
+					ipsum reprehenderit possimus ut, at aspernatur expedita
+					ullam, fugiat laudantium repellendus cumque. Tempore
+					aspernatur et porro delectus vel doloribus nemo. Debitis,
+					similique ipsam. Culpa fugiat, corrupti laborum quis
+					voluptatem quidem quibusdam veniam voluptate ipsum ea non
+					excepturi impedit, doloremque beatae praesentium esse iure
+					accusantium nam natus. Impedit soluta reprehenderit ex odit
+					cupiditate recusandae delectus ullam nihil. Facilis,
+					eligendi ullam quam tempora suscipit qui alias debitis, at
+					reiciendis dolores corporis recusandae sit beatae
+					praesentium? Quia odio harum voluptates. Dolore hic vel
+					nulla nesciunt aut iste, rerum, eos dolorem tenetur quos
+					perferendis adipisci labore unde eius at et? Ad, molestias.
+					Deserunt accusamus, rem, natus, eius molestiae nemo fugit
+					doloremque eos ut minus doloribus.
+				</p>
+			</div>
+
+			<div
+				className="my-32 max-w-xl mx-auto h-96 bg-slate-300 bg-contain bg-no-repeat bg-center"
+				style={{
+					backgroundImage: `url(./src/assets/1.jpg)`,
 				}}
 			></div>
 		</div>
